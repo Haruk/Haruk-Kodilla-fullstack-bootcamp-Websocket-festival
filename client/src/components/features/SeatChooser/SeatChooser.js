e0cabf2f061815df342a890c849e1dd15a4f448d
Aleksandra <haruka.lex@gmail.com>
2023-03-11 19:24:11 +0100

    New version project. MongoDB connected.

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -1,56 +1,96 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Progress, Alert } from 'reactstrap';
-import { getSeats, loadSeats, loadSeatsRequest, getRequests } from '../../../redux/seatsRedux';
-import io from 'socket.io-client';
+import {
+  getSeats,
+  loadSeatsRequest,
+  getRequests,
+  loadSeats,
+} from '../../../redux/seatsRedux';
 import './SeatChooser.scss';
+import io from 'socket.io-client';
 
 const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
-  const maxSeats = 50;
-
   const dispatch = useDispatch();
   const seats = useSelector(getSeats);
   const requests = useSelector(getRequests);
-  const [socket, setSocket] = useState(null);
-
-  const getSeatTeakenNumber = (seats) => seats.filter(seat => seat.day === chosenDay ? true : false).length;
+  const SEATS_NUMBER = 50;
 
   useEffect(() => {
-    if (socket) {
-      socket.on(`seatsUpdated`, (seats) => dispatch(loadSeats(seats)));
-    }
-
     dispatch(loadSeatsRequest());
 
-  }, [dispatch, socket])
+    const socket = io(
+      process.env.NODE_ENV === 'production' ? '' : 'localhost:8000',
+      { transports: ['websocket'] }
+    );
 
-
-  useEffect(() => {
-    const socket = io(`localhost:8000`);
-    setSocket(socket)
-  }, [])
+    socket.on('seatsUpdated', (seatsServer) => {
+      console.log('seats received from the server');
+      dispatch(loadSeats(seatsServer));
+    });
+  }, [dispatch]);
 
   const isTaken = (seatId) => {
-    return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
-  }
+    return seats.some((item) => item.seat === seatId && item.day === chosenDay);
+  };
 
   const prepareSeat = (seatId) => {
-    if (seatId === chosenSeat) return <Button key={seatId} className="seats__seat" color="primary">{seatId}</Button>;
-    else if (isTaken(seatId)) return <Button key={seatId} className="seats__seat" disabled color="secondary">{seatId}</Button>;
-    else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
-  }
+    if (seatId === chosenSeat)
+      return (
+        <Button key={seatId} className='seats__seat' color='primary'>
+          {seatId}
+        </Button>
+      );
+    else if (isTaken(seatId))
+      return (
+        <Button key={seatId} className='seats__seat' disabled color='secondary'>
+          {seatId}
+        </Button>
+      );
+    else
+      return (
+        <Button
+          key={seatId}
+          color='primary'
+          className='seats__seat'
+          outline
+          onClick={(e) => updateSeat(e, seatId)}
+        >
+          {seatId}
+        </Button>
+      );
+  };
 
   return (
     <div>
       <h3>Pick a seat</h3>
-      <small id="pickHelp" className="form-text text-muted ml-2"><Button color="secondary" /> – seat is already taken</small>
-      <small id="pickHelpTwo" className="form-text text-muted ml-2 mb-4"><Button outline color="primary" /> – it's empty</small>
-      {(requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(maxSeats)].map((x, i) => prepareSeat(i + 1))}</div>}
-      {(requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={ maxSeats} />}
-      {(requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert>}
-      <div>Ilosc Miejsc: {getSeatTeakenNumber(seats)} / {maxSeats}</div>
+      <small id='pickHelp' className='form-text text-muted ml-2'>
+        <Button color='secondary' /> – seat is already taken
+      </small>
+      <small id='pickHelpTwo' className='form-text text-muted ml-2 mb-4'>
+        <Button outline color='primary' /> – it's empty
+      </small>
+      {requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success && (
+        <div className='seats'>
+          {[...Array(SEATS_NUMBER)].map((x, i) => prepareSeat(i + 1))}
+          <p>
+            Free seats:
+            {SEATS_NUMBER -
+              seats.filter(
+                (element) => Number(element.day) === Number(chosenDay)
+              ).length}
+            /{SEATS_NUMBER}
+          </p>
+        </div>
+      )}
+      {requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending && (
+        <Progress animated color='primary' value={50} />
+      )}
+      {requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error && (
+        <Alert color='warning'>Couldn't load seats...</Alert>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default SeatChooser;
